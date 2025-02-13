28f23bdad9012d684d42140460dee8f853c9d1c0
Atish <atishjadhav835@gmail.com>
2025-02-13 13:40:42 +0530

    BI1.1_HW2_Done

---
+++ BI1.1_HW2/src/App.jsx
@@ -0,0 +1,14 @@
+import Hotels from './components/Hotels'
+import HotelByTitle from './components/HotelByTitle'
+
+function App() {
+  
+  return (
+    <>
+      <Hotels />
+      <HotelByTitle title="New Hotel1"/>
+    </>
+  )
+}
+
+export default App
